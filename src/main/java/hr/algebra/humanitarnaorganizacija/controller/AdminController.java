ed18ae6f4b3652f92e17ac5b39962248d6418479
Pino <pbandaleet@gmail.com>
2026-08-17 18:02:58 +0200

    ManageController + AdminFunctionality Finished

--- src/main/java/hr/algebra/humanitarnaorganizacija/controller/AdminController.java
+++ src/main/java/hr/algebra/humanitarnaorganizacija/controller/AdminController.java
@@ -12,16 +12,12 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
-import java.util.concurrent.ThreadPoolExecutor;
-
 
 public class AdminController {
 
