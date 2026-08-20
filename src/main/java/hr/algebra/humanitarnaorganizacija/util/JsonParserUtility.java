f02bf934cef8baa25f4585ca1ef97fa8ffe9d11a
Pino <pbandaleet@gmail.com>
2026-08-20 13:02:59 +0200

    JSON parser Utility mechanism

--- src/main/java/hr/algebra/humanitarnaorganizacija/util/JsonParserUtility.java
+++ src/main/java/hr/algebra/humanitarnaorganizacija/util/JsonParserUtility.java
@@ -1,20 +1,14 @@
 package hr.algebra.humanitarnaorganizacija.util;
-
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hr.algebra.humanitarnaorganizacija.exception.ApiException;
 import hr.algebra.humanitarnaorganizacija.model.Country;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.lang.classfile.instruction.NewMultiArrayInstruction;
 import java.util.ArrayList;
 import java.util.List;
 
-public class JsonParserUtility {
+public final class JsonParserUtility {
 
     /// LOGGER ////
     private static final Logger log = LoggerFactory.getLogger(JsonParserUtility.class);
@@ -30,6 +24,34 @@ public class JsonParserUtility {
         List<Country> countries = new ArrayList<>();
         try {
             JsonNode root = MAPPER.readTree(json);
+            if(!root.has("error")) {
+                String msg = "API hasn't returned expected JSON shape";
+                log.error(msg);
+                throw new ApiException(msg);
+            }
+
+            boolean error = root.path("error").asBoolean(false);
+
+            if (error) {
+                String msg = "Countries NOW API error: " +  root.path("msg").asText("Unknown ERROR");
+                log.error(msg);
+                throw new ApiException(msg);
+            }
+            JsonNode data = root.path("data");
+
+            if (!data.isArray()) {
+                String msg = "Countries NOW did not return data as ArrayList";
+                log.error(msg);
+                throw new ApiException(msg);
+            }
+
+            for(JsonNode node: data) {
+                String stateName = node.path("country").asText("").trim();
+                if(!stateName.isBlank()) {
+                    countries.add(new Country(stateName));
+                }
+            }
+
         } catch (ApiException e) {
             throw e;
         } catch (Exception e) {
